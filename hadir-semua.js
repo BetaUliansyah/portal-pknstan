d24bb9a8e3c8d2352499c94f0ade11221424d01f
Beta Uliansyah <beta.uliansyah@gmail.com>
2022-03-29 10:29:28 +0700

    Create hadir-semua.js

---
+++ hadir-semua.js
@@ -0,0 +1,6 @@
+// Buka Halaman Portal PKN STAN yang berisi presensi mahasiswa
+// Ctrl I untuk membuka halaman Inspect, klik tab Console, copaskan code berikut
+for (var i = 0; i < document.getElementsByClassName('yes').length
+; i++) {
+	document.getElementsByClassName('yes')[i].checked = true;
+}
